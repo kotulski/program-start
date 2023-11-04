01485cc582eeb0f41502c29bf5b0af2322f2ba24
Patryk <patrykos138@gmail.com>
2023-11-04 11:23:48 +0100

    .

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -3,26 +3,21 @@ using System.IO;
 
 internal class Program
 {
+    static int a = 0, b = 0;
+    static string pobrana;
     /// <summary>
     /// Procedura główna aplikacji
     /// </summary>
     /// <param name="args"> Parametry wejściowe w postaci tablicy tekstowej </param>
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, Patryk!");
-        Kielbasa();
+        Console.WriteLine("Podaj liczbę a: ");
+        
+        a = int.Parse(Console.ReadLine()!);
+
+        Console.WriteLine(pobrana);
 
         Console.ReadLine();
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    private static void Kielbasa()
-    {
-        Console.WriteLine($"Kielbasa");
-        // TextReader tr = File.OpenText("D:\\Studia\\Programowanie\\program-start\\README.md");
-        // Console.WriteLine(tr.ReadToEnd());
     }
 
 }
